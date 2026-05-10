4508432f4d7c429deecc22d37175a0d33aaa3636
vishalsekar <gv0207@gmail.com>
2026-05-10 16:06:50 +0530

    files alignment

---
+++ Git Visual Studio/Oops/_init_operations.py
@@ -0,0 +1,10 @@
+class student():
+    def __init__(self,name,id,Std):
+        self.name=name
+        self.id=id
+        self.Std=Std
+
+s1=student("vishal",12,"5th std")
+print(s1.name)
+print(s1.id)
+print(s1.Std)
